c9391464e7e759fc5131a9a2c44439b75c04b990
BrianCheng-TheLegend <briancheng1229@gmail.com>
2023-12-07 15:45:04 +0800

    Update hw02 test

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -63,53 +63,67 @@ class TestTopModule(exeFilename: String) extends Module {
   io.mem_debug_read_data    := mem.io.debug_read_data
 }
 
-class FibonacciTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior.of("Single Cycle CPU")
-  it should "recursively calculate Fibonacci(10)" in {
-    test(new TestTopModule("fibonacci.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
-      for (i <- 1 to 50) {
-        c.clock.step(1000)
-        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
-      }
+// class FibonacciTest extends AnyFlatSpec with ChiselScalatestTester {
+//   behavior.of("Single Cycle CPU")
+//   it should "recursively calculate Fibonacci(10)" in {
+//     test(new TestTopModule("fibonacci.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+//       for (i <- 1 to 50) {
+//         c.clock.step(1000)
+//         c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+//       }
 
-      c.io.mem_debug_read_address.poke(4.U)
-      c.clock.step()
-      c.io.mem_debug_read_data.expect(55.U)
-    }
-  }
-}
+//       c.io.mem_debug_read_address.poke(4.U)
+//       c.clock.step()
+//       c.io.mem_debug_read_data.expect(55.U)
+//     }
+//   }
+// }
 
-class QuicksortTest extends AnyFlatSpec with ChiselScalatestTester {
-  behavior.of("Single Cycle CPU")
-  it should "perform a quicksort on 10 numbers" in {
-    test(new TestTopModule("quicksort.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
-      for (i <- 1 to 50) {
-        c.clock.step(1000)
-        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
-      }
-      for (i <- 1 to 10) {
-        c.io.mem_debug_read_address.poke((4 * i).U)
-        c.clock.step()
-        c.io.mem_debug_read_data.expect((i - 1).U)
-      }
-    }
-  }
-}
+// class QuicksortTest extends AnyFlatSpec with ChiselScalatestTester {
+//   behavior.of("Single Cycle CPU")
+//   it should "perform a quicksort on 10 numbers" in {
+//     test(new TestTopModule("quicksort.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+//       for (i <- 1 to 50) {
+//         c.clock.step(1000)
+//         c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+//       }
+//       for (i <- 1 to 10) {
+//         c.io.mem_debug_read_address.poke((4 * i).U)
+//         c.clock.step()
+//         c.io.mem_debug_read_data.expect((i - 1).U)
+//       }
+//     }
+//   }
+// }
 
-class ByteAccessTest extends AnyFlatSpec with ChiselScalatestTester {
+// class ByteAccessTest extends AnyFlatSpec with ChiselScalatestTester {
+//   behavior.of("Single Cycle CPU")
+//   it should "store and load a single byte" in {
+//     test(new TestTopModule("sb.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+//       for (i <- 1 to 500) {
+//         c.clock.step()
+//         c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+//       }
+//       c.io.regs_debug_read_address.poke(5.U) // t0
+//       c.io.regs_debug_read_data.expect(0xdeadbeefL.U)
+//       c.io.regs_debug_read_address.poke(6.U) // t1
+//       c.io.regs_debug_read_data.expect(0xef.U)
+//       c.io.regs_debug_read_address.poke(1.U) // ra
+//       c.io.regs_debug_read_data.expect(0x15ef.U)
+//     }
+//   }
+// }
+
+class Hw02Test extends AnyFlatSpec with ChiselScalatestTester {
   behavior.of("Single Cycle CPU")
-  it should "store and load a single byte" in {
-    test(new TestTopModule("sb.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+  it should "Run the Hw02 Test." in {
+    test(new TestTopModule("hw02.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
       for (i <- 1 to 500) {
-        c.clock.step()
+        c.clock.step(1000)
         c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
       }
-      c.io.regs_debug_read_address.poke(5.U) // t0
-      c.io.regs_debug_read_data.expect(0xdeadbeefL.U)
-      c.io.regs_debug_read_address.poke(6.U) // t1
-      c.io.regs_debug_read_data.expect(0xef.U)
-      c.io.regs_debug_read_address.poke(1.U) // ra
-      c.io.regs_debug_read_data.expect(0x15ef.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(0.U)
     }
   }
 }
